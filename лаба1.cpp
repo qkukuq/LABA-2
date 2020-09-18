55549db6867914b7673c479aa0476fef350a9fa4
qkukuq <71485231+qkukuq@users.noreply.github.com>
2020-09-18 09:37:16 +0300

    Add files via upload

---
+++ лаба1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &a[i]);
+	}
+	int b[10]{};
+	for (int i = 0; i < 10; i += 2) {
+		b[i + 1] = a[i];
+    }
+	for (int i = 1; i < 10; i += 2) {
+		b[i - 1] = a[i];
+	}
+	for(int i = 0; i<10; i++){ 
+		printf("%d ", b[i]); 
+	}
+	return 0;
+}
+
+
